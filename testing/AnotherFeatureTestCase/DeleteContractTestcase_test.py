97b7dfc5c81b0511c9f74a12f25e3f71640a7dda
fixxall <faturrohmansugiyarto@gmail.com>
2024-02-05 21:32:29 +0700

    ads

--- testing/AnotherFeatureTestCase/DeleteContractTestcase_test.py
+++ testing/AnotherFeatureTestCase/DeleteContractTestcase_test.py
@@ -15,61 +15,61 @@ class LogoutTestCase(unittest.TestCase):
         except:
             cls.url = "http://localhost"
 
-    def test_start(self):
-        def login_correct_credentials(self):
-            login_url = self.url +'/login.php'
-            self.browser.get(login_url)
-            self.browser.find_element(By.ID, 'inputUsername').send_keys('admin')
-            self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
-            self.browser.find_element(By.TAG_NAME, 'button').click()
+    def login_correct_credentials(self):
+        login_url = self.url +'/login.php'
+        self.browser.get(login_url)
+        self.browser.find_element(By.ID, 'inputUsername').send_keys('admin')
+        self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
+        self.browser.find_element(By.TAG_NAME, 'button').click()
 
-        def index_page(self):
-            expected_result = "admin"
-            actual_result = self.browser.find_element(By.XPATH, "//h2[contains(text(),'Halo,')]").text.split(', ')[1]
-            self.assertIn(expected_result, actual_result)
+    def index_page(self):
+        expected_result = "admin"
+        actual_result = self.browser.find_element(By.XPATH, "//h2[contains(text(),'Halo,')]").text.split(', ')[1]
+        self.assertIn(expected_result, actual_result)
+    
+    def create_contact_page(self):
+        expected_result = "contact"
+        self.browser.find_element(By.XPATH, "/html/body/div[1]/div[1]/div/a").click()
+        actual_result = self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/h5").text.split('new')[1]
+        self.assertIn(expected_result, actual_result)
         
-        def create_contact_page(self):
-            expected_result = "contact"
-            self.browser.find_element(By.XPATH, "/html/body/div[1]/div[1]/div/a").click()
-            actual_result = self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/h5").text.split('new')[1]
-            self.assertIn(expected_result, actual_result)
-            
-        def create_contact_form(self):
-            self.browser.find_element(By.ID, 'name').send_keys('mufat1')
-            self.browser.find_element(By.ID, 'email').send_keys('mufat1@gmail.com')
-            self.browser.find_element(By.ID, 'phone').send_keys('08111111111111111')
-            self.browser.find_element(By.ID, 'title').send_keys('Teman1')
-            self.browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/form/input[5]').click()
-            index_page_title = "Dashboard"
-            actual_title = self.browser.title
-            self.assertEqual(index_page_title, actual_title)
+    def create_contact_form(self):
+        self.browser.find_element(By.ID, 'name').send_keys('mufat1')
+        self.browser.find_element(By.ID, 'email').send_keys('mufat1@gmail.com')
+        self.browser.find_element(By.ID, 'phone').send_keys('08111111111111111')
+        self.browser.find_element(By.ID, 'title').send_keys('Teman1')
+        self.browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/form/input[5]').click()
+        index_page_title = "Dashboard"
+        actual_title = self.browser.title
+        self.assertEqual(index_page_title, actual_title)
 
-        def search_contact_name(self):
-            self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys('mufat1')
-            self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(Keys.ENTER)
-            searched_contact_name = self.browser.find_elements(By.XPATH, f"//td[contains(text(), 'mufat1')]")
-            self.assertTrue(searched_contact_name)
+    def search_contact_name(self):
+        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys('mufat1')
+        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(Keys.ENTER)
+        searched_contact_name = self.browser.find_elements(By.XPATH, f"//td[contains(text(), 'mufat1')]")
+        self.assertTrue(searched_contact_name)
 
-        def delete_contact(self):
-            actions_section = self.browser.find_element(By.XPATH, "//tr[@role='row'][1]//td[contains(@class, 'actions')]")
-            delete_button = actions_section.find_element(By.XPATH, ".//a[contains(@class, 'btn-danger')]")
-            delete_button.click()
-            self.browser.switch_to.alert.accept()
+    def delete_contact(self):
+        actions_section = self.browser.find_element(By.XPATH, "//tr[@role='row'][1]//td[contains(@class, 'actions')]")
+        delete_button = actions_section.find_element(By.XPATH, ".//a[contains(@class, 'btn-danger')]")
+        delete_button.click()
+        self.browser.switch_to.alert.accept()
 
-        def check_search_contact_name(self):
-            self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys('mufat1')
-            self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(Keys.ENTER)
-            searched_contact_name = self.browser.find_elements(By.XPATH, f"//td[contains(text(), 'mufat1')]")
-            self.assertFalse(searched_contact_name)
+    def check_search_contact_name(self):
+        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys('mufat1')
+        self.browser.find_element(By.ID, 'employee_filter').find_element(By.TAG_NAME, 'input').send_keys(Keys.ENTER)
+        searched_contact_name = self.browser.find_elements(By.XPATH, f"//td[contains(text(), 'mufat1')]")
+        self.assertFalse(searched_contact_name)
 
-        login_correct_credentials(self)
-        index_page(self)
-        create_contact_page(self)
-        create_contact_form(self)
-        search_contact_name(self)
-        delete_contact(self)
+    def test_start(self):
+        self.login_correct_credentials(self)
+        self.index_page(self)
+        self.create_contact_page(self)
+        self.create_contact_form(self)
+        self.search_contact_name(self)
+        self.delete_contact(self)
         time.sleep(1)
-        check_search_contact_name(self)
+        self.check_search_contact_name(self)
 
     @classmethod
     def tearDownClass(cls):
